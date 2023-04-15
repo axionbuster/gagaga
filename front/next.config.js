a1a46acf96050af98fe88d916850a3115509454a
YuJin Kim <axionbuster@gmail.com>
2023-04-15 16:39:19 -0700

    front: introduce Next.js (SPA mode)

---
+++ front/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  // Use single page app
+  // https://nextjs.org/docs/advanced-features/static-html-export
+  output: 'export',
+}
+
+module.exports = nextConfig
